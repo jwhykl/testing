8e377ef2b56c445a4626a04a96ea57d944352377
John <j-why@outlook.com>
2020-04-16 15:25:08 +0800

    Changes for Etl

--- exercises/etl/etl.py
+++ exercises/etl/etl.py
@@ -1,2 +1,11 @@
+import string
+
 def transform(legacy_data):
-    pass
+    output={}
+    for key, ld in legacy_data.items():
+        for ld_i in ld:
+            try:
+                output[ld_i.lower()]=key
+            except Exception as error:
+                raise Exception("No matching key-value pair ", repr(error))
+    return output
